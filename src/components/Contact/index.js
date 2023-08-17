b15f8d05b5456d98454570cea6bad5564bea2dda
benjamin verdès <verdesb.devacc@gmail.com>
2023-08-17 15:42:10 +0200

    contact done

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,12 +1,19 @@
-import './contact.scss'
+import './contact.scss';
+import github from '../../assets/icon/Github.svg';
+import linkedin from '../../assets/icon/Linkedin.svg';
 
 const Contact = () => {
     return (
         <section className="contact" id="contact">
+            <div className='contact__title'>
+                <h2>Contact Me</h2>
+                <h4>I won't bite</h4>
+            </div>
+            <div className='contact__container'>
             <div className="contact__socials">
                 <ul>
-                    <li><a><img></img></a></li>
-                    <li><a><img></img></a></li>
+                    <li><a href='#'><img src={github}></img></a></li>
+                    <li><a href='#'><img src={linkedin}></img></a></li>
                 </ul>
 
             </div>
@@ -19,9 +26,10 @@ const Contact = () => {
                                     <label for="mail">Adresse e-mail:</label>
                                         <input type="email" id="mail" name="mail"></input>
                                             <label for="message">Message:</label>
-                                                <textarea id="message" name="message"></textarea>
+                                                <textarea rows={10} cols={17} id="message" name="message"></textarea>
                                                     <input type="submit" value="Envoyer"></input>
                                                     </form>
+                                                </div>
                                                 </div>
 
 
