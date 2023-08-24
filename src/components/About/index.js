b31d5a0163e1abfa5cc240a44021180a539c27ef
benjamin verdès <verdesb.devacc@gmail.com>
2023-08-24 14:53:31 +0200

    test btn about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -11,7 +11,9 @@ const About = () => {
                 <h2>About Me</h2>
                 <h5>Je suis développeur web polyvalent, avec un max de créativité</h5>
                 <p>Je suis un développeur web fullstack passionné et ingénieux, capable de m’adapter rapidement à de nouveaux environnements et défis grâce à mes compétences en soft skills. Je suis capable de concevoir, développer et maintenir des applications web en utilisant une variété de technologies et de langages de programmation. Mon expertise en développement front-end et back-end me permet de créer des expériences utilisateur fluides et engageantes tout en garantissant la stabilité et la sécurité des applications. Je suis constamment à la recherche de nouveaux défis pour améliorer mes compétences et contribuer à des projets passionnants.</p>
-                <button className='about__btn'>Hire Me </button>
+                <div className='about__btnbox'>
+                <button type='button' className='about__btn' href="/">Hire Me </button>
+                </div>
             </div>
         </section>
     )
